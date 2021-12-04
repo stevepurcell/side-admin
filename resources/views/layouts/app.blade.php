35d95a66ece210060cc3217d7889ef543aa2369e
Steve Purcell <sm_purcell@yahoo.com>
2021-12-03 20:53:38 -0600

    First Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+  <x-header />
+  <body>
+    <div x-data="setup()" x-init="$refs.loading.classList.add('hidden'); setColors(color);" :class="{ 'dark': isDark}">
+      <div class="flex h-screen antialiased text-gray-900 bg-gray-100 dark:bg-dark dark:text-light">
+        <!-- Loading screen -->
+        <div
+          x-ref="loading"
+          class="fixed inset-0 z-50 flex items-center justify-center text-2xl font-semibold text-white bg-primary-darker"
+        >
+          Loading.....
+        </div>
+
+        <!-- Sidebar -->
+        <x-sidebar />
+
+        <div class="flex-1 h-full overflow-x-hidden overflow-y-auto">
+          <!-- Navbar -->
+          <x-navbar />
+
+          <!-- Main content -->
+          <main>
+            <!-- Content header -->
+            <div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
+              <h1 class="text-2xl font-semibold">Dashboard</h1>
+              
+            </div>
+
+            {{ $slot }}
+          </main>
+
+          <!-- Main footer -->
+          <footer
+            class="flex items-center justify-between p-4 bg-white border-t dark:bg-darker dark:border-primary-darker"
+          >
+            <div>K-WD &copy; 2021</div>
+            <div>
+              Made by
+              <a href="https://github.com/Kamona-WD" target="_blank" class="text-blue-500 hover:underline"
+                >Ahmed Kamel</a
+              >
+            </div>
+          </footer>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- All javascript code in this project for now is just for demo DON'T RELY ON IT  -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.bundle.min.js"></script>
+    <script src="build/js/script.js"></script>
+    <script>
+      const setup = () => {
+        const getTheme = () => {
+          if (window.localStorage.getItem('dark')) {
+            return JSON.parse(window.localStorage.getItem('dark'))
+          }
+
+          return !!window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+        }
+
+        const setTheme = (value) => {
+          window.localStorage.setItem('dark', value)
+        }
+
+        const getColor = () => {
+          if (window.localStorage.getItem('color')) {
+            return window.localStorage.getItem('color')
+          }
+          return 'cyan'
+        }
+
+        const setColors = (color) => {
+          const root = document.documentElement
+          root.style.setProperty('--color-primary', `var(--color-${color})`)
+          root.style.setProperty('--color-primary-50', `var(--color-${color}-50)`)
+          root.style.setProperty('--color-primary-100', `var(--color-${color}-100)`)
+          root.style.setProperty('--color-primary-light', `var(--color-${color}-light)`)
+          root.style.setProperty('--color-primary-lighter', `var(--color-${color}-lighter)`)
+          root.style.setProperty('--color-primary-dark', `var(--color-${color}-dark)`)
+          root.style.setProperty('--color-primary-darker', `var(--color-${color}-darker)`)
+          this.selectedColor = color
+          window.localStorage.setItem('color', color)
+          //
+        }
+
+        const updateBarChart = (on) => {
+          const data = {
+            data: randomData(),
+            backgroundColor: 'rgb(207, 250, 254)',
+          }
+          if (on) {
+            barChart.data.datasets.push(data)
+            barChart.update()
+          } else {
+            barChart.data.datasets.splice(1)
+            barChart.update()
+          }
+        }
+
+        const updateDoughnutChart = (on) => {
+          const data = random()
+          const color = 'rgb(207, 250, 254)'
+          if (on) {
+            doughnutChart.data.labels.unshift('Seb')
+            doughnutChart.data.datasets[0].data.unshift(data)
+            doughnutChart.data.datasets[0].backgroundColor.unshift(color)
+            doughnutChart.update()
+          } else {
+            doughnutChart.data.labels.splice(0, 1)
+            doughnutChart.data.datasets[0].data.splice(0, 1)
+            doughnutChart.data.datasets[0].backgroundColor.splice(0, 1)
+            doughnutChart.update()
+          }
+        }
+
+        const updateLineChart = () => {
+          lineChart.data.datasets[0].data.reverse()
+          lineChart.update()
+        }
+
+        return {
+          loading: true,
+          isDark: getTheme(),
+          toggleTheme() {
+            this.isDark = !this.isDark
+            setTheme(this.isDark)
+          },
+          setLightTheme() {
+            this.isDark = false
+            setTheme(this.isDark)
+          },
+          setDarkTheme() {
+            this.isDark = true
+            setTheme(this.isDark)
+          },
+          color: getColor(),
+          selectedColor: 'cyan',
+          setColors,
+          toggleSidbarMenu() {
+            this.isSidebarOpen = !this.isSidebarOpen
+          },
+          isSettingsPanelOpen: false,
+          openSettingsPanel() {
+            this.isSettingsPanelOpen = true
+            this.$nextTick(() => {
+              this.$refs.settingsPanel.focus()
+            })
+          },
+          isNotificationsPanelOpen: false,
+          openNotificationsPanel() {
+            this.isNotificationsPanelOpen = true
+            this.$nextTick(() => {
+              this.$refs.notificationsPanel.focus()
+            })
+          },
+          isSearchPanelOpen: false,
+          openSearchPanel() {
+            this.isSearchPanelOpen = true
+            this.$nextTick(() => {
+              this.$refs.searchInput.focus()
+            })
+          },
+          isMobileSubMenuOpen: false,
+          openMobileSubMenu() {
+            this.isMobileSubMenuOpen = true
+            this.$nextTick(() => {
+              this.$refs.mobileSubMenu.focus()
+            })
+          },
+          isMobileMainMenuOpen: false,
+          openMobileMainMenu() {
+            this.isMobileMainMenuOpen = true
+            this.$nextTick(() => {
+              this.$refs.mobileMainMenu.focus()
+            })
+          },
+          updateBarChart,
+          updateDoughnutChart,
+          updateLineChart,
+        }
+      }
+    </script>
+  </body>
+</html>
